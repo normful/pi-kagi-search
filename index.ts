086ef28b31cea1827751db0f0cfbbf0babd68789
Norman Sue <normful@users.noreply.github.com>
2026-03-27 00:36:18 +0900

    fix: remove unused import

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import type { ExtensionAPI, ExtensionContext } from "@mariozechner/pi-coding-agent";
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
 import { Text } from "@mariozechner/pi-tui";
 import { Type } from "@sinclair/typebox";
 import { parse, HTMLElement } from "node-html-parser";
